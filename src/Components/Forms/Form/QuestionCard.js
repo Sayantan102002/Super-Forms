59b5f2695666e783659214786d585627647458ac
Sayantan102002 <62782051+Sayantan102002@users.noreply.github.com>
2023-03-15 03:26:41 +0530

    Question Card style modernised

--- src/Components/Forms/Form/QuestionCard.js
+++ src/Components/Forms/Form/QuestionCard.js
@@ -6,22 +6,28 @@ import QuestionSelect from "./QuestionSelect";
 import ComponentRenderer from "./Component.render";
 
 export default function QuestionCard() {
-    const [val,setValue]=React.useState(2);
+  const [val, setValue] = React.useState(2);
   return (
     <Paper
       component="form"
       sx={{
-        "& > :not(style)": { m: 1, width: "25ch" },
-        
+        // "& > :not(style)": { m: 1, width: "25ch" },
+        width: "40%",
+        border: '1px solid red',
+        padding: "20px"
       }}
       noValidate
       autoComplete="off"
     >
-        <Box sx={{ display: "flex", minWidth: "100vw", justifyContent: "space-around"}}>
-      <TextField id="outlined-basic" label="Name" variant="standard" />
-      <QuestionSelect val={val} setValue={setValue}/>
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ width: "50%" }}>
+          <TextField id="outlined-basic" label="Name" variant="filled" fullWidth sx={{ m: "1" }} />
         </Box>
-<ComponentRenderer val={val}/>
-   </Paper>
+        <Box>
+          <QuestionSelect val={val} setValue={setValue} />
+        </Box>
+      </Box>
+      <ComponentRenderer val={val} />
+    </Paper>
   );
 }
--- src/Components/Forms/Form/Questions.js
+++ src/Components/Forms/Form/Questions.js
@@ -1,13 +1,14 @@
+import { Box } from '@mui/material'
 import React from 'react'
 import QuestionCard from './QuestionCard'
 
 const Questions = () => {
   return (
-    <>
-      <QuestionCard/>
-      
+    <Box sx={{ display: 'flex', flexDirection: 'column', border: '1px solid blue', alignItems: 'center' }}>
+      <QuestionCard />
 
-    </>
+
+    </Box>
   )
 }
 
