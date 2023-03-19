ed9b7e1e31f3b8cce04fa00d4f707d971fd9a08a
Sayantan102002 <62782051+Sayantan102002@users.noreply.github.com>
2023-03-19 16:14:19 +0530

    Build bug fixed

--- src/Components/Helper/FormTab.js
+++ src/Components/Helper/FormTab.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { appleTabsStylesHook } from '@mui-treasury/styles/tabs';
-import { Tab, Tabs } from '@mui/material';
+// import React from 'react';
+// import { appleTabsStylesHook } from '@mui-treasury/styles/tabs';
+// import { Tab, Tabs } from '@mui/material';
 
-const AppleTabs = () => {
-    const [tabIndex, setTabIndex] = React.useState(0);
-    const tabsStyles = appleTabsStylesHook.useTabs();
-    const tabItemStyles = appleTabsStylesHook.useTabItem();
-    return (
-        <Tabs
-            classes={tabsStyles}
-            value={tabIndex}
-            onChange={(e, index) => setTabIndex(index)}
-        >
-            <Tab classes={tabItemStyles} disableRipple label={'All'} />
-            <Tab classes={tabItemStyles} disableRipple label={'Missed'} />
-            <Tab classes={tabItemStyles} disableRipple label={'Calls'} />
-        </Tabs>
-    );
-};
+// const AppleTabs = () => {
+//     const [tabIndex, setTabIndex] = React.useState(0);
+//     const tabsStyles = appleTabsStylesHook.useTabs();
+//     const tabItemStyles = appleTabsStylesHook.useTabItem();
+//     return (
+//         <Tabs
+//             classes={tabsStyles}
+//             value={tabIndex}
+//             onChange={(e, index) => setTabIndex(index)}
+//         >
+//             <Tab classes={tabItemStyles} disableRipple label={'All'} />
+//             <Tab classes={tabItemStyles} disableRipple label={'Missed'} />
+//             <Tab classes={tabItemStyles} disableRipple label={'Calls'} />
+//         </Tabs>
+//     );
+// };
 
-export default AppleTabs;
+// export default AppleTabs;
