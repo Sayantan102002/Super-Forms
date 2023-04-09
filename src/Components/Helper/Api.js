66dd899ed099584b8b24af9a5f07d1881874f2ea
Sayantan102002 <62782051+Sayantan102002@users.noreply.github.com>
2023-04-09 13:35:13 +0530

    Production env setup fixed

--- src/Components/Helper/Api.js
+++ src/Components/Helper/Api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 // import config from '../config/index';
 let Api = null;
 
-Api = axios.create({ baseURL: 'http://localhost:5000/' });
+// Api = axios.create({ baseURL: 'http://localhost:5000/' });
+Api = axios.create({ baseURL: 'https://super-forms.onrender.com' });
 
 
 
