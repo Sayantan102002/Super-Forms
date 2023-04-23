c711f28f5c6db2aa23e45063ecf6b74f7f0ab4ca
mr07s <106861707+mr07s@users.noreply.github.com>
2023-04-23 12:33:20 +0530

    login page designed

--- src/Components/Auth/actionCodeSettings.js
+++ src/Components/Auth/actionCodeSettings.js
@@ -1,7 +1,8 @@
 export const actionCodeSettings = {
     // URL you want to redirect back to. The domain (www.example.com) for this
     // URL must be in the authorized domains list in the Firebase Console.
-    url: 'https://super-forms-aa3c8.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
+    // url: 'https://super-forms-aa3c8.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
+    url: 'http://localhost:3000/dashboard',
     handleCodeInApp: true,
     // iOS: {
     //     bundleId: 'com.example.ios'
